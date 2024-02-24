c765dca993dce2eedcd0188bf0ce74faf5fcfc05
Dmytro Oleksandryuk <dima.oleksandryuk@gmail.com>
2024-02-25 00:12:21 +0200

    Initial commit

---
+++ src/main/java/dimaoleks/CrossPlatform_Lab3/CrossPlatformLab3Application.java
@@ -0,0 +1,13 @@
+package dimaoleks.CrossPlatform_Lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPlatformLab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dimaoleks/CrossPlatform_Lab3/CrossPlatformLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dimaoleks.CrossPlatform_Lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatformLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
